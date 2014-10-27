7bac7ab3d42e2a50f52db12581b767a04928e481
Jordan Scales <jordan@khanacademy.org>
2014-10-27 16:55:51 -0700

    displaying actual activity in the graphs

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 $(function () {
-  $(".card .activity").each(function () {
-    var lineData = [];
-    var $bar;
+    $(".card").each(function () {
+        var repo = $(this).data("repo");
+        var url = "https://api.github.com/repos/Khan/" + repo + "/stats/commit_activity";
+        var $activity = $(this).find(".activity");
 
-    var padding = 1; // 2px
-    var count = 52;
-    var containerWidth = $(this).outerWidth();
-    var width = (containerWidth - (count - 1) * padding) / count;
+        $.getJSON(url, function (data) {
+            var activityByWeek = data.map(function (week) {
+                return week.total
+            });
+            var maxActivity = Math.max.apply(null, activityByWeek);
 
-    for (var i = 0; i < count; i++) {
-      height = Math.floor(Math.random() * 100);
-      $bar = $("<div></div>")
-        .addClass("bar")
-        .css("left", (width + padding) * i + "px")
-        .css("width", width)
-        .css("height", height + "px")
+            var padding = 1;
+            var count = activityByWeek.length;
+            var containerWidth = $activity.outerWidth();
+            var width = (containerWidth - (count - 1) * padding) / count;
 
-      $(this).append($bar)
-    }
-  });
+            activityByWeek.forEach(function (week, i) {
+                var height = Math.floor(week / maxActivity * 100);
+                var $bar = $("<div></div>")
+                    .addClass("bar")
+                    .css("left", (width + padding) * i + "px")
+                    .css("width", width)
+                    .css("height", height + "px")
+
+                $activity.append($bar)
+            });
+        });
+    });
 });
