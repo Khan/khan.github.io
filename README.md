e82d3060d572e0378c8a2034a8713718d62a720a
Jeremy Wiebe <jeremy.wiebe@gmail.com>
2022-05-10 12:09:25 -0700

    Update Perseus logo, use Lato, simplify (#18)
    ## Summary:
    
    We recently resumed work on Perseus. The original logo was a hand holding a sword. We decided to use a less violent logo and designed one that represents teh constellation Perseus. 
    
    I've also taken the liberty to update the page to use WB/brand colors and switch to Lato which is our current KA font.
    
    Issue: LP-11682
    
    <img width="500" alt="Screen Shot 2022-05-10 at 11 30 41 AM" src="https://user-images.githubusercontent.com/77138/167698353-114de718-add8-4821-9792-2c2146893076.png">
    
    ## Test plan:
    
    Review the look of the page
    
    Author: jeremywiebe
    
    Reviewers: jeremywiebe, megaroeny, nixterrimus, jeresig, kevinbarabash
    
    Required Reviewers:
    
    Approved By: nixterrimus
    
    Checks: ❌ Mixed content, ❌ Redirect rules, ❌ Header rules, ❌ Pages changed
    
    Pull Request URL: https://github.com/Khan/khan.github.io/pull/18

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 We generate it using a couple of Handlebars templates found in the "templates" folder from data found in [data.js](./data.js).
 
-##
+## Building
+
 To re-build the site:
 
 ```
-npm install
-npm run build
+yarn install
+yarn run build
 ```
--- data.js
+++ data.js
@@ -4,111 +4,141 @@
  */
 var entries = [
     // Flagship projects
-{
-    projectSlug: "aphrodite",
-    projectName: "Aphrodite",
-    language: "JavaScript",
-    description: "Inline styles that work!"
-}, {
-    projectSlug: "katex",
-    projectName: "KaTeX",
-    language: "JavaScript",
-    description: "The fastest math rendering library for the web."
-}, {
-    projectSlug: "live-editor",
-    projectName: "Live Editor",
-    language: "JavaScript",
-    description: "A browser-based live coding environment."
-}, {
-    projectSlug: "perseus",
-    projectName: "Perseus",
-    language: "JavaScript",
-    description: "Khan Academy's exercise question editor and renderer."
-}, {
-    projectSlug: "tota11y",
-    projectName: "Tota11y",
-    language: "JavaScript",
-    description: "An accessibility (a11y) visualization toolkit."
-}, {
-    projectSlug: "swifttweaks",
-    projectName: "SwiftTweaks",
-    language: "Swift",
-    description: "Adjust your iOS app on-device, without needing to recompile."
-}, {
-    projectSlug: "kmath",
-    projectName: "Kmath",
-    language: "JavaScript",
-    description: "JavaScript Numeric Math Utilities."
-},
+    {
+        projectSlug: "aphrodite",
+        projectName: "Aphrodite",
+        language: "JavaScript",
+        projectLogo: "./img/aphrodite.svg",
+        description: "Inline styles that work!"
+    }, {
+        projectSlug: "katex",
+        projectName: "KaTeX",
+        language: "JavaScript",
+        projectLogo: "./img/katex.png",
+        description: "The fastest math rendering library for the web."
+    }, {
+        projectSlug: "live-editor",
+        projectName: "Live Editor",
+        language: "JavaScript",
+        projectLogo: "./img/live-editor.png",
+        description: "A browser-based live coding environment."
+    }, {
+        projectSlug: "perseus",
+        projectName: "Perseus",
+        language: "JavaScript",
+        projectLogo: "./img/perseus.png",
+        description: "Khan Academy's exercise question editor and renderer."
+    }, {
+        projectSlug: "tota11y",
+        projectName: "Tota11y",
+        language: "JavaScript",
+        projectLogo: "./img/tota11y.png",
+        description: "An accessibility (a11y) visualization toolkit."
+    }, {
+        projectSlug: "perseus/tree/main/packages/kmath",
+        projectName: "Kmath",
+        language: "JavaScript",
+        projectLogo: "./img/kmath.svg",
+        description: "JavaScript Numeric Math Utilities."
+    }, {
+        projectSlug: "wonder-blocks",
+        projectName: "Wonder Blocks",
+        language: "JavaScript",
+        projectLogo: "./img/wonder-blocks.svg",
+        description: "React components for Wonder Blocks design system."
+    },
     // Some new projects, which we'd like to be closer to the top
-{
-    projectSlug: "genqlient",
-    projectName: "genqlient",
-    language: "Go",
-    description: "A truly type-safe GraphQL client for Go."
-}, {
-    projectSlug: "mu-lambda",
-    projectName: "Mu Lambda",
-    language: "JavaScript",
-    description: "A small library of functional programming utilities."
-}, {
-    projectSlug: "react-balance-text",
-    projectName: "React Balance Text",
-    language: "JavaScript",
-    description: "A React wrapper for the Adobe Web Platform's Balance-Text Project"
-}, {
-    projectSlug: "fuzzy-match-utils",
-    projectName: "Fuzzy Match Utils",
-    language: "JavaScript",
-    description: "A collection of string matching algorithms designed with React Select in mind."
-}, {
-    projectSlug: "react-multi-select",
-    projectName: "React Multi-Select",
-    language: "JavaScript",
-    description: "A multiple select component for React"
-},
-    // ALl the rest of the projects
-{
-    projectSlug: "react-components",
-    projectName: "React Components",
-    language: "JavaScript",
-    description: "Some components we build for Khan Academy the world might find useful."
-}, {
-    projectSlug: "snippets",
-    projectName: "Snippets",
-    language: "Python",
-    description: "Code related to collecting and pushing weekly snippets."
-}, {
-    projectSlug: "khan-api",
-    projectName: "Khan API",
-    language: "JavaScript",
-    description: "Documentation for (and examples of) using the Khan Academy API."
-}, {
-    projectSlug: "tinyquery",
-    projectName: "TinyQuery",
-    language: "Python",
-    description: "A Python in-memory test stub for BigQuery."
-}, {
-    projectSlug: "gae_mini_profiler",
-    projectName: "GAE Mini Profiler",
-    language: "JavaScript",
-    description: "A ubiquitous mini-profiler for Google App Engine, inspired by mvc-mini-profiler."
-}, {
-    projectSlug: "i18nize_templates",
-    projectName: "i18nize Templates",
-    language: "Python",
-    description: "A tool to automatically add i18n markup to jinja2 and handlebars templates."
-}, {
-    projectSlug: "guacamole",
-    projectName: "Guacamole",
-    language: "Python",
-    description: "General-use machine learning for learning library."
-}, {
-    projectSlug: "structuredjs",
-    projectName: "Structured JS",
-    language: "JavaScript",
-    description: "A simple interface for checking the structure of JavaScript code."
-}
+    {
+        projectSlug: "genqlient",
+        projectName: "genqlient",
+        language: "Go",
+        description: "A truly type-safe GraphQL client for Go."
+    }, {
+        projectSlug: "mu-lambda",
+        projectName: "Mu Lambda",
+        language: "JavaScript",
+        projectLogo: "./img/Mu-lambda.png",
+        description: "A small library of functional programming utilities."
+    }, {
+        projectSlug: "react-balance-text",
+        projectName: "React Balance Text",
+        language: "JavaScript",
+        projectLogo: "./img/react-balance-text.png",
+        description: "A React wrapper for the Adobe Web Platform's Balance-Text Project"
+    }, {
+        projectSlug: "fuzzy-match-utils",
+        projectName: "Fuzzy Match Utils",
+        language: "JavaScript",
+        projectLogo: "./img/fuzzy-match-utils.png",
+        description: "A collection of string matching algorithms designed with React Select in mind."
+    }, {
+        projectSlug: "react-multi-select",
+        projectName: "React Multi-Select",
+        language: "JavaScript",
+        projectLogo: "./img/react-multi-select.png",
+        description: "A multiple select component for React"
+    }, {
+        projectSlug: "perseus/tree/main/packages/math-input",
+        projectName: "Math Input",
+        language: "JavaScript",
+        description: "An expression editor for the mobile web."
+    },
+    // All the rest of the projects
+    {
+        projectSlug: "react-components",
+        projectName: "React Components",
+        language: "JavaScript",
+        projectLogo: "./img/react-components.png",
+        description: "Some components we build for Khan Academy the world might find useful."
+    }, {
+        projectSlug: "snippets",
+        projectName: "Snippets",
+        language: "Python",
+        projectLogo: "./img/snippets.png",
+        description: "Code related to collecting and pushing weekly snippets."
+    }, {
+        projectSlug: "khan-api",
+        projectName: "Khan API",
+        language: "JavaScript",
+        projectLogo: "./img/khan-api.png",
+        description: "Documentation for (and examples of) using the Khan Academy API."
+    }, {
+        projectSlug: "tinyquery",
+        projectName: "TinyQuery",
+        language: "Python",
+        projectLogo: "./img/tinyquery.png",
+        description: "A Python in-memory test stub for BigQuery."
+    }, {
+        projectSlug: "gae_mini_profiler",
+        projectName: "GAE Mini Profiler",
+        language: "JavaScript",
+        projectLogo: "./img/gae_mini_profiler.png",
+        description: "A ubiquitous mini-profiler for Google App Engine, inspired by mvc-mini-profiler."
+    }, {
+        projectSlug: "i18nize_templates",
+        projectName: "i18nize Templates",
+        language: "Python",
+        projectLogo: "./img/i18nize_templates.png",
+        description: "A tool to automatically add i18n markup to jinja2 and handlebars templates."
+    }, {
+        projectSlug: "guacamole",
+        projectName: "Guacamole",
+        language: "Python",
+        projectLogo: "./img/guacamole.png",
+        description: "General-use machine learning for learning library."
+    }, {
+        projectSlug: "structuredjs",
+        projectName: "Structured JS",
+        language: "JavaScript",
+        projectLogo: "./img/structuredjs.png",
+        description: "A simple interface for checking the structure of JavaScript code."
+    }, {
+        projectSlug: "swifttweaks",
+        projectName: "SwiftTweaks",
+        language: "Swift",
+        projectLogo: "./img/swifttweaks.png",
+        description: "Adjust your iOS app on-device, without needing to recompile."
+    }
 ];
 
 module.exports = entries;
